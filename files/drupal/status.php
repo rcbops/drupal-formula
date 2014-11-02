8477cad85a70bf91b0ba1c21b5a25942b65fe057
James Absalon <james.absalon@rackspace.com>
2014-11-01 23:04:36 -0500

    Initial commit in new repo.

---
+++ files/drupal/status.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>Drupal Health Check</title>
+    </head>
+    <body>
+    <?php print "I'm alive, but I haven't tested the drupal installation or db connectivity."?>
+    </body>
+</html>
